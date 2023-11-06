207a8b617b24624b0f33ee5cf7f3fef9f8eccf07
Andrew Gierens <apgierens@gmail.com>
2023-11-07 08:17:15 +1100

    Ignore test file

--- tessie_api/tessie_wrapper.py
+++ tessie_api/tessie_wrapper.py
@@ -1,16 +1,19 @@
 import aiohttp
-import aiohttp
 from typing import Any, Dict, Optional
 
 
-async def tessieRequest(session: aiohttp.ClientSession, method: str, path: str, api_key: str, params: Optional[Dict[str, Any]] = None) -> Dict[str, Any]:
+async def tessieRequest(
+    session: aiohttp.ClientSession,
+    method: str,
+    path: str,
+    api_key: str,
+    params: Optional[Dict[str, Any]] = None,
+) -> Dict[str, Any]:
     url = f"https://api.tessie.com{path}"
     headers = {
         "Authorization": f"Bearer {api_key}",
         "Content-Type": "application/json",
     }
-    async with session.request(
-        method, url, headers=headers, params=params
-    ) as response:
+    async with session.request(method, url, headers=headers, params=params) as response:
         response.raise_for_status()  # Will raise aiohttp.ClientResponseError on 4xx or 5xx
         return await response.json()
